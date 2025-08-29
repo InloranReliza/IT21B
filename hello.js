6fa39c2212cef93c72c79bd10d8f3be0fea3e057
Inloran Reliza <inloranreliza@gmail.com>
2025-08-29 08:35:51 +0800

    added subtraction,multiplication and division

--- hello.js
+++ hello.js
@@ -3,4 +3,11 @@ console.log("hello world");
 n1 =2;
 n2 =3;
 sum =n1+ n2;
+diff =n1-n2;
+prod =n1 * n2;
+quo = n1 /n2 ;
+
 console.log ("The sum of "+ n1 +"and" + n2 +"is"+sum);
+console.log ("The difference of "+n1+"&"+n2+"is"+diff);
+console.log ("The product of  "+ n1 +"&" + n2 +"is"+prod);
+console.log ("The quotient of "+n1+"&"+n2+"is"+quo);
