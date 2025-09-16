793fe1726866ea31a697a89cf64a5eca430543bf
InloranReliza <inloranreliza@gmail.com>
2025-09-16 08:18:26 +0800

    Added warrior class as inherited from Hero class

--- hero.js
+++ hero.js
@@ -22,11 +22,15 @@ class Hero{
         console.log ("-------------------");
     }
 }
-
-const hero1 = new Hero("Thorin", 100, 10);
+class Warrior extends  Hero {
+     useAbility(){
+        console.log ('$this.getName()+" uses Power Strike');
+        }
+}
+const hero1 = new Warrior("Thorin", 100, 10);
 const hero2 = new Hero("Gandalf", 100, 8);
  
 
-hero1.showStats();
-hero1.showStats();
+Thorin.useAbility();
+
 
